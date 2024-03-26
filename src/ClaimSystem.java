8da082cf44593e1985c331a0481f07779d96feb6
Shrimpy <s3978724@rmit.edu.vn>
2024-03-27 03:25:05 +0700

    initialize

--- src/ClaimSystem.java
+++ src/ClaimSystem.java
@@ -2,7 +2,14 @@
  * @author Nguyen Ich Kiet - s3978724
  */
 import java.util.*;
-public class ClaimManagementSystem {
+import com.google.gson.Gson;
+public class ClaimSystem {
     private List<Claim> claimList;
     private List<Customer> customerList;
+
+    public void readData() {
+        Gson gson = new Gson();
+
+
+    }
 }
---
+++ src/MenuManager.java
@@ -0,0 +1,5 @@
+/**
+ * @author Nguyen Ich Kiet - s3978724
+ */
+public class MenuManager {
+}
