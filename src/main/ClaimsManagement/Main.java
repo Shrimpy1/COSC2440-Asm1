ba1e8e0afcac91dd8c4648b30000f5552b2dade4
Shrimpy <s3978724@rmit.edu.vn>
2024-04-06 04:22:04 +0700

    update main to implement system

--- src/main/ClaimsManagement/Main.java
+++ src/main/ClaimsManagement/Main.java
@@ -9,6 +9,7 @@ import model.customer.CustomerSet;
 import model.customer.Dependant;
 import model.insurance_card.InsuranceCard;
 import model.insurance_card.InsuranceCardSet;
+import system.ClaimSystem;
 import view.claim.ClaimConsoleView;
 import view.customer.CustomerConsoleView;
 import view.customer.DependantConsoleView;
@@ -17,62 +18,9 @@ import view.insurance_card.InsuranceCardConsoleView;
 
 public class Main {
     public static void main(String[] args) {
-//        InsuranceCard i1 = new InsuranceCard();
-//
-//        FileHandler fh = FileHandler.getInstance();
-//
-//        fh.writeData("InsuranceCard", i1);
-//        Dependant d1 = new Dependant("123", "John");
-//        PolicyHolder p1 = new PolicyHolder("123", "Son");
-//
-//        System.out.println(d1.equals(p1));
-//        PolicyHolder kiet = new PolicyHolder("123", "kiet");
-//        Dependant tan = new Dependant("012", "tan");
-//        kiet.addDependant(tan);
-//
-//        CustomerSet.getInstance().add(kiet);
-//        CustomerSet.getInstance().add(tan);
-//
-//        CustomerSet.getInstance().saveData();
-
-        CustomerSet.getInstance();
-        InsuranceCardSet.getInstance();
-        ClaimSet.getInstance();
-
-//        DependantConsoleView dview = new DependantConsoleView();
-//        PolicyholderConsoleView pview = new PolicyholderConsoleView();
-//        System.out.println(CustomerSet.getInstance().getCustomers().size());
-//        CustomerSet.getInstance().getCustomers().forEach(customer -> {
-//            if (customer instanceof Dependant){
-//                dview.display(customer);
-//            } else {
-//                pview.display(customer);
-//            }
-//        });
-//
-//        InsuranceCardController icc = new InsuranceCardController(new InsuranceCard(), new InsuranceCardConsoleView());
-//
-////        icc.createNewInsuranceCard();
-//
-//        for (InsuranceCard card : InsuranceCardSet.getInstance().getInsuranceCards()){
-//            icc.getView().display(card);
-//        }
-
-        ClaimController cc = new ClaimController(new Claim(), new ClaimConsoleView());
-//        cc.createNewClaim();
-
-        System.out.println(ClaimSet.getInstance().getClaims().size());
-        for (Claim claim : ClaimSet.getInstance().getClaims()){
-            cc.getView().display(claim);
-        }
-
-        CustomerSet.getInstance().saveData();
-        InsuranceCardSet.getInstance().saveData();
-        ClaimSet.getInstance().saveData();
-//
-//        FileHandler.getInstance().writeObjectToFile("customer", CustomerSet.getInstance().getCustomers());
-//        FileHandler.getInstance().writeObjectToFile("insurance_card.txt", InsuranceCardSet.getInstance().getInsuranceCards());
-
-//        CustomerSet.getInstance().getCustomers().forEach(customer -> );
+        ClaimSystem system = new ClaimSystem();
+        system.initialize();
+        system.run();
+        system.saveData();
     }
 }
